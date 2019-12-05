eb5dda6b7292996ece86b21c34633c4e907a3fe3
yu-1111 <2570304823@qq.com>
2019-12-05 11:44:35 +0800

    yzw

---
+++ src/components/yuzw/Friends.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="frimode">
+    <!-- 1 -->
+    <div class="coteng">
+      <div class="single-friend">
+        <div class="friend-box">
+          <div class="friend-photo-box">
+            <div class="friend-p-wrap">
+              <img src="../../images/y-zhi/1.png" onerror="this.src='../../images/y-zhi/1.png'">
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+    <!-- 2 -->
+    <div class="coteng">
+      <div class="single-friend">
+        <div class="friend-box">
+          <div class="friend-photo-box">
+            <div class="friend-p-wrap">
+              <img src="../../images/y-zhi/1.png" onerror="this.src='../../images/y-zhi/1.png'">
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+    <!-- 2 -->
+    <div class="coteng">
+      <div class="single-friend">
+        <div class="friend-box">
+          <div class="friend-photo-box">
+            <div class="friend-p-wrap">
+              <img src="../../images/y-zhi/1.png" onerror="this.src='../../images/y-zhi/1.png'">
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+    <!-- 2 -->
+    <div class="coteng">
+      <div class="single-friend">
+        <div class="friend-box">
+          <div class="friend-photo-box">
+            <div class="friend-p-wrap">
+              <img src="../../images/y-zhi/1.png" onerror="this.src='../../images/y-zhi/1.png'">
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less'>
+@import '../../css/Friend.less';
+@import '../../css/public.css';
+</style>
