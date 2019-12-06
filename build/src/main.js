ff2efbc1140508055747b68805c0d60821a4fa70
liulong520 <2418661044@qq.com>
2019-12-06 17:45:54 +0800

    刘龙

---
+++ build/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 公共样式
+import 'css/public.less'
+// 字体图标引入
+import 'css/font-awesome/css/font-awesome.min'
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
