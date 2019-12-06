a35444aeb4afdee75c100c8b05326e0c4c9cb8cf
wangwengnag <930836043@qq.com>
2019-12-06 18:16:13 +0800

    王文刚12.6

--- src/components/gallery/BrideGroom.vue
+++ src/components/gallery/BrideGroom.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>
+<div>
+  <div class="Brid-groom-content">
+    <div class="Brid-groom-area">
+      <div class="container">
+        <div class="container-wrap">
+          <h1>Bride and Groom</h1>
+          <ul class="container-wrap-lists clearfix">
+            <li class="container-wrap-list fl margin-r-5">
+              <router-link to="/home">Home</router-link>
+            </li>
+            <li class="container-wrap-list active fl margin-l-5">Bride & Groom</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
+  <BrideBanner></BrideBanner>
+  <PublicTab></PublicTab>
+</div>
 </template>
 
 <script>
+import PublicTab from './publicTab.vue'
+import BrideBanner from './Bridebanner.vue'
 export default {
-
+  components: {
+    PublicTab,
+    BrideBanner
+  }
 }
 </script>
 
-<style>
+<style lang='less' scope>
+@import '../../css/public.less';
+@import '../../css/gallery.less';
 
 </style>
