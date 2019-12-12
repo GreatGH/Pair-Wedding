8e0328dd75b50c3f4a94f982bf428c62fca1b64b
yu-1111 <2570304823@qq.com>
2019-12-12 11:22:46 +0800

    余

--- src/components/friends/Rsvp.vue
+++ src/components/friends/Rsvp.vue
@@ -4,14 +4,16 @@
         <div class="setion-over">
             <div class="container">
                 <div class="title">
-                  <div class="text-center">
-                  <div class="main-title">RSVP</div>
-                  <div class="home-locations">
-                      <router-link to="/home" class="home">Home</router-link>
-                      <span>/</span>
-                      <router-link to="/rsvp" class="location">RSVP</router-link>
-                  </div>
-                  </div>
+                  <Bounin>
+                    <div class="text-center" v-if="show" slot="bounceIn">
+                    <div class="main-title">RSVP</div>
+                      <div class="home-locations">
+                          <router-link to="/home" class="home">Home</router-link>
+                          <span>/</span>
+                          <router-link to="/rsvp" class="location">RSVP</router-link>
+                      </div>
+                    </div>
+                  </Bounin>
               </div>
             </div>
         </div>
@@ -24,14 +26,19 @@
 
 <script>
 import FormModel from '../home/formModel.vue'
+import Bounin from 'comp/location/slot/BounceIn'
 export default {
   data () {
     return {
-
+      show: false
     }
   },
   components: {
-    FormModel
+    FormModel,
+    Bounin
+  },
+  mounted () {
+    this.show = true
   }
 }
 </script>
